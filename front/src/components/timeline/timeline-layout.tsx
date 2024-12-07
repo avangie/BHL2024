fc0c36297b8fe8e36d008492c85816c68adbac3c
Jakub Prokopiuk <kubapro84@wp.pl>
2024-12-07 22:22:37 +0100

    changed how timeline behaves

--- front/src/components/timeline/timeline-layout.tsx
+++ front/src/components/timeline/timeline-layout.tsx
@@ -17,41 +17,25 @@ import { TimelineElement } from "@/app/data";
 interface TimelineLayoutProps {
     items: TimelineElement[];
 }
+
 export const TimelineLayout = ({ items }: TimelineLayoutProps) => {
     return (
         <Timeline>
-            <TimelineItem>
-                <TimelineConnector />
-                <TimelineHeader>
-                    <TimelineTime>{items[0].date}</TimelineTime>
-                    <TimelineIcon />
-                    <TimelineTitle>{items[0].title}</TimelineTitle>
-                </TimelineHeader>
-                <TimelineContent>
-                    <TimelineDescription>{items[0].description}</TimelineDescription>
-                </TimelineContent>
-            </TimelineItem>
-            <TimelineItem>
-                <TimelineConnector />
-                <TimelineHeader>
-                    <TimelineTime>{items[1].date}</TimelineTime>
-                    <TimelineIcon />
-                    <TimelineTitle>{items[1].title}</TimelineTitle>
-                </TimelineHeader>
-                <TimelineContent>
-                    <TimelineDescription>{items[1].description}</TimelineDescription>
-                </TimelineContent>
-            </TimelineItem>
-            <TimelineItem>
-                <TimelineHeader>
-                    <TimelineTime>{items[2].date}</TimelineTime>
-                    <TimelineIcon />
-                    <TimelineTitle>{items[2].title}</TimelineTitle>
-                </TimelineHeader>
-                <TimelineContent>
-                    <TimelineDescription>{items[2].description}</TimelineDescription>
-                </TimelineContent>
-            </TimelineItem>
+            {items.map((item) => {
+                return (
+                    <TimelineItem key={item.id}>
+                        {items.indexOf(item) < items.length - 1 && <TimelineConnector />}
+                        <TimelineHeader>
+                            <TimelineTime>{item.date}</TimelineTime>
+                            <TimelineIcon />
+                            <TimelineTitle>{item.title}</TimelineTitle>
+                        </TimelineHeader>
+                        <TimelineContent>
+                            <TimelineDescription>{item.description}</TimelineDescription>
+                        </TimelineContent>
+                    </TimelineItem>
+                );
+            })}
         </Timeline>
     );
 };
